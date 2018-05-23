72378e94f0152ff082e2954168aa40b61d39e4da
Garrett Kizior <kiziorgarrett@gmail.com>
2018-05-23 12:32:01 -0500

    changed schema from skill -> skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WMP Skill Tracker';
+
+  private apiUrl = 'http://localhost:8080';
+
+  data: any;
+
+  constructor(private http: Http) {
+    this.getAll();
+  }
+
+  getAll() {
+    return this.http
+      .get(this.apiUrl + '/getAll')
+      .pipe(map((res: Response) => res.json()))
+      .subscribe(data => {
+        this.data = data;
+      });
+  }
+
+  delete() {
+    return this.http
+      .get(this.apiUrl + '/delete')
+      .pipe(map((res: Response) => res.text()))
+      .subscribe(data => {
+        console.log(data);
+        this.getAll();
+      });
+  }
+
+  save() {
+    return this.http
+      .get(this.apiUrl + '/save')
+      .pipe(map((res: Response) => res.text()))
+      .subscribe(data => {
+        console.log(data);
+        this.getAll();
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
