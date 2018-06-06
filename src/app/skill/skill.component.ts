ff92adab62ffa18e13dd80b6d206c1168d587d25
Garrett Kizior <kiziorgarrett@gmail.com>
2018-06-06 16:11:06 -0500

    assign new skill to selected emps

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -5,6 +5,8 @@ import { AppService } from '../app.service';
 import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 
 @Component({
   selector: 'app-skill',
@@ -26,11 +28,22 @@ export class SkillComponent implements OnInit {
 
   chartData: any;
 
+  emps: any;
+
+  empOptions: IMultiSelectOption[] = [];
+  searchSettings: any;
+
   constructor(
     private appService: AppService,
     private http: Http,
-    private httpclient: HttpClient
+    private httpclient: HttpClient,
+    private modalService: NgbModal
   ) {
+    this.appService.pageTitle = 'Skill';
+    this.searchSettings = {
+      enableSearch: true,
+      pullRight: appService.isRTL
+    };
     this.chartData = [
       {
         name: '',
@@ -42,16 +55,35 @@ export class SkillComponent implements OnInit {
   ngOnInit() {
     this.uniqueSkills();
     this.getSkillGraph();
+    this.getAll();
   }
 
   newSkill() {
-    const skillObject = new Object();
-    skillObject['skill'] = this.skill;
-    return this.httpclient
-      .post(this.apiUrl + '/api/skills', skillObject, this.options)
-      .subscribe(result => {
-        this.getSkillGraph();
-      });
+    if (this.skill === undefined || this.skill === null || this.skill === '') {
+      alert('Need to input a skill');
+      return;
+    }
+    if (this.emps !== undefined && this.emps !== []) {
+      const empIdsObject = new Object();
+      empIdsObject['empIds'] = this.emps;
+      return this.httpclient
+        .post(
+          this.apiUrl + '/api/skills/create/' + this.skill,
+          empIdsObject,
+          this.options
+        )
+        .subscribe(result => {
+          this.getSkillGraph();
+        });
+    } else {
+      const skillObject = new Object();
+      skillObject['skill'] = this.skill;
+      return this.httpclient
+        .post(this.apiUrl + '/api/skills', skillObject, this.options)
+        .subscribe(result => {
+          this.getSkillGraph();
+        });
+    }
   }
 
   uniqueSkills() {
@@ -70,10 +102,46 @@ export class SkillComponent implements OnInit {
       });
   }
 
+  getAll() {
+    return this.http
+      .get(this.apiUrl + '/getAll')
+      .pipe(map((res: Response) => res.json()))
+      .subscribe(data => {
+        this.empOptions = [];
+        for (let i = 0; i < data.length; i++) {
+          this.empOptions[i] = {
+            id: data[i].empId,
+            name: data[i].firstName + ' ' + data[i].lastName
+          };
+        }
+      });
+  }
+
   removeAll(skill) {
     console.log(skill);
     return this.httpclient
       .delete(this.apiUrl + '/api/skills/removeAll/' + skill, this.options)
       .subscribe();
+  }
+
+  open(content, options = {}) {
+    this.modalService.open(content, options).result.then(
+      result => {
+        console.log(`Closed with: ${result}`);
+      },
+      reason => {
+        console.log(`Dismissed ${this.getDismissReason(reason)}`);
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
